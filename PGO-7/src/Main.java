80530f465e91b4972b927800b2e093312e329fff
s30349 <s30349@pjwstk.edu.pl>
2024-05-05 19:31:38 +0200

    zadanie-7

--- PGO-7/src/Main.java
+++ PGO-7/src/Main.java
@@ -1,15 +1,46 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//zadanie.1
+interface Figure{
+    double getPerimeter();
+    double getArea();
+    String getType();
+}
+
+//zadanie.2
+interface Moveable{
+    void start();
+    void stop();
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //zadanie.1
+        double radius = 3d;
+        Figure circle = new Circle(radius);
+        double baseSide = 10d;
+        double side = 5d;
+        double height = 8d;
+        Figure parallelogram = new Parallelogram(baseSide, side, height);
+        double sideA = 15d;
+        double sideB = 10d;
+        Figure rectangle = new Rectangle(sideA, sideB);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        ArrayList<Figure> figures = new ArrayList<>(3);
+        figures.add(circle);
+        figures.add(parallelogram);
+        figures.add(rectangle);
 
-
+        for (Figure figure : figures) {
+            System.out.println(figure.getType());
+            System.out.println("Area: " + figure.getArea() + " units.");
+            System.out.println("Perimeter: " + figure.getPerimeter() + " units.\n");
         }
+
+        //zadanie.2
+        Cat mruczek = new Cat("Mruczek");
+        mruczek.start();
+        System.out.println(mruczek.getType());
+        mruczek.stop();
+
     }
-}
+
+    }
